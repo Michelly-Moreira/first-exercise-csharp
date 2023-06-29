a1fac3e1282dfa8efc1b9ecf89de5f17cb7d3870
Michelly Moreira <michelly.daiana@hotmail.com>
2023-06-28 21:41:05 -0300

    Retornando Olá, Mundo!

--- src/ola-mundo/HelloWorld.cs
+++ src/ola-mundo/HelloWorld.cs
@@ -7,9 +7,10 @@ public class HelloWorld
     // 1 - Retorne a string 'Olá, Mundo!'
     public static string Greet()
     {
-        throw new NotImplementedException();
+        string salutation = "Olá, Mundo!";
+        return salutation;
     }
-    // 2 - Retorne a string 'Seja bem-vindo, Tryber!
+    // 2 - Retorne a string 'Seja bem-vindo, Tryber!'
     public static string GreetTryber()
     {
         throw new NotImplementedException();
